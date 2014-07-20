b3b27d57f2dd9ebf7f2a6ea0ff4fdd46d64a54f0
JMRosenberg <Jacob.Rosenberg@Tufts.edu>
2014-07-20 17:14:14 -0400

    bodyparser, about, tweaks

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 var express = require("express");
+var mongo = require("mongodb");
+var bodyParser = require("body-parser");
 var app = express();
+
+var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/mydb';
+
+app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
     res.send('Hello World!');
+});
+
+app.post('/', function(req, res) {
+    console.log(req.body);
+    res.send('Deez Nutz!');
 });
 
 var port = Number(process.env.PORT || 5000);
